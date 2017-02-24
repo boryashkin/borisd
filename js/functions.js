039f197b62b4153a1bbacbb13d80aa3fb254efd8
boryashkin <mail@borisd.ru>
2017-02-24 11:47:28 +0500

    /about: food stat declensions

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
-function readTextFile(file, callback) {
+function readTextFile(file, callback)
+{
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
@@ -8,4 +9,20 @@ function readTextFile(file, callback) {
         }
     }
     rawFile.send(null);
+}
+
+function declensionForNumber(nominativeWord, genitiveSingularWord, genitivePluralWord, quantity)
+{
+    var nominativeCaseQts = {"1":true},//именительный
+        genitiveQts = {"2":true, "3":true, "4":true};//родительный
+
+    var lastDigitOfQty = quantity % 10;
+
+    if (quantity != 11 && nominativeCaseQts[lastDigitOfQty] === true) {
+        return nominativeWord;
+    } else if ((quantity < 11 || quantity > 20)&& genitiveQts[lastDigitOfQty] === true) {
+        return genitiveSingularWord;
+    } else {
+        return genitivePluralWord;
+    }
 }
